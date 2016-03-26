a374ae9f77006a3ceeacdfd60dc9a7bd92a75471
Ken Sodemann <ken.sodemann@gmail.com>
2016-03-26 08:22:54 -0500

    chore(release): update version

--- src/app/common/core/ClientVersion.js
+++ src/app/common/core/ClientVersion.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('homeTrax.common.core.ClientVersion', []).constant('ClientVersion', {
-    id: '2.0.0',
-    name: 'Fervent Yokel (2.0.0)',
-    releaseDate: '2016-03-11',
+    id: '2.0.1',
+    name: 'Iron Sentiment (2.0.1)',
+    releaseDate: '2016-03-26',
     copyright: '2016 Kenneth W. Sodemann'
   });
 }());
