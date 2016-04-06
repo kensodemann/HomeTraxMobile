66bdf9393c991b9be455a2b9908b061891ae614a
Ken Sodemann <ken.sodemann@gmail.com>
2016-04-06 13:48:07 -0500

    chore(build): update the version

--- src/app/common/core/ClientVersion.js
+++ src/app/common/core/ClientVersion.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('homeTrax.common.core.ClientVersion', []).constant('ClientVersion', {
-    id: '2.0.1',
-    name: 'Iron Sentiment (2.0.1)',
-    releaseDate: '2016-03-26',
+    id: '2.0.2',
+    name: 'Sultry Scholar (2.0.2)',
+    releaseDate: '2016-04-05',
     copyright: '2016 Kenneth W. Sodemann'
   });
 }());
