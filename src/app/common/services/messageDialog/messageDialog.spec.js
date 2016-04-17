0465c23d6d3fca6c81c7046239a76b0075dc7f57
Ken Sodemann <ken.sodemann@gmail.com>
2016-04-17 07:52:36 -0500

    feat(messageDialog): create ask and error dialogs

--- src/app/common/services/messageDialog/messageDialog.spec.js
+++ src/app/common/services/messageDialog/messageDialog.spec.js
@@ -4,25 +4,28 @@
 
   describe('homeTrax.common.services.messageDialog', function() {
     var messageDialog;
-    var mockLog;
+    var mockIonicPopup;
+    var showDfd;
 
     beforeEach(module('homeTrax.common.services.messageDialog'));
 
     beforeEach(function() {
-      mockLog = sinon.stub({
-        error: function() {
+      mockIonicPopup = sinon.stub({
+        show: function() {
         }
       });
     });
 
     beforeEach(function() {
       module(function($provide) {
-        $provide.value('$log', mockLog);
+        $provide.value('$ionicPopup', mockIonicPopup);
       });
     });
 
-    beforeEach(inject(function(_messageDialog_) {
+    beforeEach(inject(function($q, _messageDialog_) {
       messageDialog = _messageDialog_;
+      showDfd = $q.defer();
+      mockIonicPopup.show.returns(showDfd.promise);
     }));
 
     it('exists', function() {
@@ -30,10 +33,40 @@
     });
 
     describe('error', function() {
-      it('just logs the error for now', function() {
-        messageDialog.error('Title', 'Message');
-        expect(mockLog.error.calledOnce).to.be.true;
-        expect(mockLog.error.calledWith('Message')).to.be.true;
+      it('shows a popup', function() {
+        messageDialog.error('I am Title', 'This is some information');
+        expect(mockIonicPopup.show.calledOnce).to.be.true;
+      });
+
+      it('Passes the title', function() {
+        messageDialog.error('I am Title', 'This is some information');
+        var config = mockIonicPopup.show.firstCall.args[0];
+        expect(config.title).to.equal('I am Title');
+      });
+
+      it('Passes the message on the scope', function() {
+        messageDialog.error('I am Title', 'This is some information');
+        var config = mockIonicPopup.show.firstCall.args[0];
+        expect(config.scope.message).to.equal('This is some information');
+      });
+    });
+
+    describe('ask', function() {
+      it('shows a popup', function() {
+        messageDialog.ask('I am Title', 'This is some information');
+        expect(mockIonicPopup.show.calledOnce).to.be.true;
+      });
+
+      it('sets the title', function() {
+        messageDialog.ask('I am Title', 'This is some information');
+        var config = mockIonicPopup.show.firstCall.args[0];
+        expect(config.title).to.equal('I am Title');
+      });
+
+      it('passes the message on the scope', function() {
+        messageDialog.ask('I am Title', 'This is some information');
+        var config = mockIonicPopup.show.firstCall.args[0];
+        expect(config.scope.message).to.equal('This is some information');
       });
     });
   });
