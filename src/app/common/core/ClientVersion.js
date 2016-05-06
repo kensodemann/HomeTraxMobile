62599009823c643a5ecff916e0da5e2747fa189d
Ken Sodemann <ken.sodemann@gmail.com>
2016-05-05 20:42:09 -0600

    chore(release): update the version information

--- src/app/common/core/ClientVersion.js
+++ src/app/common/core/ClientVersion.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('homeTrax.common.core.ClientVersion', []).constant('ClientVersion', {
-    id: '2.0.2',
-    name: 'Sultry Scholar (2.0.2)',
-    releaseDate: '2016-04-05',
+    id: '2.0.3',
+    name: 'Honorable Peculiarity (2.0.3)',
+    releaseDate: '2016-05-05',
     copyright: '2016 Kenneth W. Sodemann'
   });
 }());
