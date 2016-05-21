fb215a67739552997355304102d964b481a4cebc
Ken Sodemann <ken.sodemann@gmail.com>
2016-05-20 21:25:28 -0500

    chore(release): update the version information

--- src/app/common/core/ClientVersion.js
+++ src/app/common/core/ClientVersion.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('homeTrax.common.core.ClientVersion', []).constant('ClientVersion', {
-    id: '2.0.3',
-    name: 'Honorable Peculiarity (2.0.3)',
-    releaseDate: '2016-05-05',
+    id: '2.0.4',
+    name: 'Twin Courtyard (2.0.4)',
+    releaseDate: '2016-05-21',
     copyright: '2016 Kenneth W. Sodemann'
   });
 }());
